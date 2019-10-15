58ed793fba14b4316016a6d2d871c41a42f72fdb
Taufan Fajrin <yuuki.kanagi@gmail.com>
2019-10-15 11:40:25 +0700

    Skor Basket

--- app/src/main/java/com/iren/helloworld/MainActivity.java
+++ app/src/main/java/com/iren/helloworld/MainActivity.java
@@ -3,12 +3,27 @@ package com.iren.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.guideline);
+    }
+
+    private int scorea = 0;
+    public void tambah3poina(View view) {
+        scorea += 3;
+        TextView textViewA = findViewById(R.id.scorea);
+        textViewA.setText(String.valueOf(scorea));
+    }
+
+    public void reset(View view) {
+        scorea = 0;
+        TextView scoreA = findViewById(R.id.scorea);
+        scoreA.setText(String.valueOf(scorea));
     }
 }
